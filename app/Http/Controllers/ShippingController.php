2ff3fd5bdfc0a6f0400af817957543d7ae110570
Aerossky <riskyradeonrx560@gmail.com>
2023-05-29 22:20:07 +0700

    Update ShippingController.php

--- app/Http/Controllers/ShippingController.php
+++ app/Http/Controllers/ShippingController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ShippingController extends Controller
 {
     //
+    function index(){
+        $response = Http::withHeaders([
+            'key' => 'b11975225db04e43ea0bf865c52d11cc'
+        ])->get('https://api.rajaongkir.com/starter/city');
+
+        $cities = $response['rajaongkir']['results'];
+
+        // dd($cities);
+        return view('cek-ongkir',['cities'=>$cities, 'ongkir' => '']);
+    }
+
+    function cekOngkir(Request $request){
+        // dd($request->all());
+        $response = Http::withHeaders([
+            'key' => 'b11975225db04e43ea0bf865c52d11cc'
+        ])->get('https://api.rajaongkir.com/starter/city');
+
+        $responseCost = Http::withHeaders([
+            'key' => 'b11975225db04e43ea0bf865c52d11cc'
+        ])->post('https://api.rajaongkir.com/starter/cost',[
+            'origin' => $request->origin,
+            'destination' => $request->destination,
+            'weight' => $request->weight,
+            'courier' => $request->courier
+        ]);
+
+        // dd($request->all());
+        $cities = $response['rajaongkir']['results'];
+        $ongkir = $responseCost['rajaongkir'];
+        return view('cek-ongkir',['cities'=>$cities, 'ongkir' => $ongkir, 'data' => $request]);
+
+    }
 }
